9ffe3d4648b40371716ba7f8f98a656f096db015
Dhyani-coder <97533534+Dhyani-coder@users.noreply.github.com>
2022-05-11 19:31:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.image.fromURL(Birth)
+
+
+
+
+	
+
+
+}
+
+function playSound(){
+     x.play();
+}
